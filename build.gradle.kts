cd60b0fa3dfa2503a0dd339b105dcdf246d30441
Artyom Nesin <artem.nesin@gmail.com>
2026-02-14 00:20:00 +0400

    chore: init gradle project with paper-api 1.21.11

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+}
+
+group = "io.nesin.voteplugin"
+version = "1.0.0"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
